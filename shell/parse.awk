49c8886a3b8f23ab5a1a11beb3a5010addc9ca38
franklyn <j20063346027@vip.qq.com>
2016-11-27 14:51:02 +0800

    all commit:

--- shell/parse.awk
+++ shell/parse.awk
@@ -1,3 +1,10 @@
-/^.+/{
-	print $9
+function parse_ls {
+
+	ls -la | grep -v total | awk '{print $1}' | while read line
+	do
+		echo $line
+	done
+
 }
+
+parse_ls
--- shell/redirect.sh
+++ shell/redirect.sh
@@ -4,15 +4,38 @@ if [ -e "fe" ]
 then
 	rm -f "fe"
 fi
-touch fe
+if [ -e "fee" ]
+then
+	rm -f "fee"
+fi
+if [ -e "be_process" ]
+then
+	rm -f "be_process"
+fi
+mkfifo fee
+
+echo "flag=1" >>be_process
+echo "while [ \$flag -eq 1 ] " >>be_process
+echo "do" >>be_process
+echo "read line <fee" >>be_process
+echo "echo \$line" >>be_process
+echo "if [ \"\$line\" == \"end_of_fifo\" ]" >>be_process
+echo "then" >>be_process
+echo "exit" >>be_process
+echo "fi" >>be_process
+echo "done" >>be_process
+
+chmod u+x be_process
+./be_process&
 
 ls -la | awk '{print $0}' | while read line
 do
 	echo $line >>fe
-	echo $line >>fe
+	echo $line >>fee
 done
 
-while read line
-do
-	echo $line
-done <fe
+echo "end_of_fifo" >>fee
+#while read line
+#do
+#	echo $line
+#done <fe
